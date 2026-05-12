9d833d6503224d133aecd706b62f7f1d151d0220
Neelima Palivela <24p31a1245@acet.ac.in>
2026-05-12 17:06:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',          
-  password: 'yourpassword', 
+  password: 'Neelima@2007', 
   database: 'crm1'
 });
 
